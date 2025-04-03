40bba6268cb172cbba835195541a724f844d903e
Alex Sabetti <59347621+AlexSabetti@users.noreply.github.com>
2025-04-03 18:40:44 -0400

    Update MapNode.gd

--- scripts/MapNode.gd
+++ scripts/MapNode.gd
@@ -1,1 +1,17 @@
+class_name MapNode
 extends Node
+
+@export var scene_path: PackedScene
+# Pointers to all surrounding nodes
+@export var north: MapNode
+@export var south: MapNode
+@export var east: MapNode
+@export var west: MapNode
+
+# booleans
+@export var active: bool = false
+@export var north_open: bool = true
+@export var south_open: bool = true
+@export var east_open: bool = true
+@export var west_open: bool = true
+
