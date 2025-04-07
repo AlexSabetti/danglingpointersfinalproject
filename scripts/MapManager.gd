cc6ca6c5f9c8c768e677eb82504441c9e845a896
AlexSabetti <Soyfalsoescapeplan@gmail.com>
2025-04-07 12:06:42 -0400

    more map management

--- scripts/MapManager.gd
+++ scripts/MapManager.gd
@@ -1,9 +1,11 @@
 class_name MapManager
 extends Node3D
 
+@export_category("Map Properties")
 @export var grid_count: int = 16 # MUST BE A ROOTABLE VALUE
 @export var cell_size = 16 # Change this if we change the room sizes
 @export var active_cell_count: int = 0 # How many cells we've already placed rooms in
+@export var required_areas: int = 4 # Story-Specific mission areas, the expansion function must place this many
 
 @export node_scene: MapNode # MUST BE FILLED IN
 @export starting_area: PackedScene # MUST BE FILLED IN
@@ -21,7 +23,7 @@ func Create_Map():
   var root_count = sqrt(grid_count)
   for i in range(0, root_count):
     for j in range(0, root_count):
-      node_cur_pos = {i * cell_size, 0, j * cell_size) # Tweak this
+      node_cur_pos = (i * cell_size, 0, j * cell_size) # Tweak this
       var scene = load(node_scene)
       var walker = scene.instantiate()
       add_child(walker)
@@ -33,13 +35,34 @@ func Create_Map():
       else :
         walker.south_node = NULL
       if j > 0:
-        get(child(i * root + j - 1).east_node = walker
+        get_child(i * root + j - 1).east_node = walker
         walker = get_child(i * root + j - 1)
   head = get_child(0)
         
     
-  head.north_node = NULL
+func start_map():
+  var rng = RandomNumberGenerator.new()
+  var starting_tile = rng.rand_range(0, grid_count)
+  var starting_node = get_child(starting_tile)
+  starting_node.active = true
   
-  head.west_node = NULL
-  head.
+  # We will instantiate the starting scene here, along with its exits
   
+  var starting_scene = starting_area.instantiate()
+  starting_node.add_child(starting_scene)
+  starting_scene.global_position = starting_node.global_position
+
+  # Don't know the specifics for how we're going to put up blockers yet, but we'll start with the bools
+
+  if(starting_node.north_node == NULL):
+    starting_node.north_open = false
+  if(starting_node.south_node == NULL):
+    starting_node.south_open = false
+  if(starting_node.east_node == NULL):
+    starting_node.east_open = false
+  if(starting_node.west_node == NULL): 
+    starting_node.west_open = false
+
+func expand_map():
+  var rng = RandomNumberGenerator.new()
+  var cur_tile =  0
