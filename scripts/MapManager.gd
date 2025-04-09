1ee106edd0c31f21b2fbf9afd7c5dd0d4c1141ec
AlexSabetti <Soyfalsoescapeplan@gmail.com>
2025-04-08 23:27:12 -0400

    Slowly getting there

--- scripts/MapManager.gd
+++ scripts/MapManager.gd
@@ -13,14 +13,23 @@ extends Node3D
 @export var expanding_tries: int = 12 # Perhaps change this to be a percentage of the grid_count
 @export var original_node_loc: Vector3 = {0, 0, 0}
 
+var starting_node: MapNode = NULL
+var starting_node_place : int = -1
+
+var key_areas: Array = []
+var points_of_interest: Array = []
+var paths: Array = []
+
+var root_count: int = -1
 var head: MapNode = NULL
 func _ready():
+  root_count = sqrt(grid_count)
   Create_Map()
 
 func Create_Map():
   var node_cur_pos: Vector3 = original_node_loc
 
-  var root_count = sqrt(grid_count)
+  
   for i in range(0, root_count):
     for j in range(0, root_count):
       node_cur_pos = (i * cell_size, 0, j * cell_size) # Tweak this
@@ -43,8 +52,9 @@ func Create_Map():
 func start_map():
   var rng = RandomNumberGenerator.new()
   var starting_tile = rng.rand_range(0, grid_count)
-  var starting_node = get_child(starting_tile)
+  starting_node = get_child(starting_tile)
   starting_node.active = true
+  starting_node_place = starting_tile
   
   # We will instantiate the starting scene here, along with its exits
   
@@ -65,4 +75,23 @@ func start_map():
 
 func expand_map():
   var rng = RandomNumberGenerator.new()
-  var cur_tile =  0
+  # There are many ways to go about doing this, but for now what I'll do is select two-three random nodes
+  # I'll label the selected ones as "key areas" and then I'll try to expand from the starting node to connect there
+  # Then I'll select a few more random nodes and label them "points of interest" and try to connect them to a key area, an already established path, or the starting area, whichever is closer
+  var first_key_area = -1
+  var second_key_area = -1
+
+  while first_key_area == second_key_area || first_key_area == starting_node_place || second_key_area == starting_node_place:
+    first_key_area = rng.rand_range(0, grid_count)
+    second_key_area = rng.rand_range(0, grid_count)
+  # On smaller maps, this might loop for a long time, so this is the first and last time we're doing this
+
+  key_areas.append(get_child(first_key_area))
+  key_areas.append(get_child(second_key_area))
+
+  var num = starting_node_place - first_key_area
+  if num < 0:
+    # Below or to the right
+    var rows_away = abs(num) / root_count
+    var cols_away = abs(num) % root_count
+
